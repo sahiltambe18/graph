826b93df546a5a37f5b437dcdc434e0adcd598c5
Sahil Tambe <sahiltambe81922@gmail.com>
2023-09-12 09:41:00 +0530

    bellman ford

---
+++ G1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int v[9] = {-1};
+
+void bfs(vector<int> g[], int l)
+{
+    queue<int> q;
+    q.push(1);
+    v[1] = 1;
+    // cout<<1<<" ";
+    cout << "bfs : ";
+    while (!q.empty())
+    {
+        int fr = q.front();
+        cout << fr << " ";
+        q.pop();
+
+        for (auto &i : g[fr])
+        {
+            if (v[i] != 1)
+            {
+                q.push(i);
+                v[i] = 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    // vector<pair<int , int>> g[6];
+    vector<int> g[9];
+    int e;
+    cout << "enter number of edges\n";
+    cin >> e;
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cout << "enter edge \n";
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    bfs(g, 6);
+
+    return 0;
+}
